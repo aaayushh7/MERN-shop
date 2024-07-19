2b93c6d77938d107cd5e077c58b9476f8b94ea1e
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-07-19 22:17:04 +0530

    lko

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://mern-shop-backend.vercel.app";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
