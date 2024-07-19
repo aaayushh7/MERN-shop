700763c83c6e177371ec90a2b48bb9f491f4a6d2
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-07-20 00:11:25 +0530

    BE_Commit_7

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,9 +7,9 @@ const generateToken = (res, userId) => {
 
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "None",
+    sameSite: null,
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
