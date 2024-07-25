bd18d1689d689b55e5354662977f5e9ccc27bc7d
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-07-25 21:35:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,17 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mern-fe-orcin.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
