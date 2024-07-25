0c49c9bba514182174510c8a68dcb42416ccfc4a
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-07-25 22:19:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,18 +22,9 @@ const app = express();
 app.use(cors({
   origin: ['https://mern-fe-orcin.vercel.app', 'http://localhost:5173'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
 }));
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://mern-fe-orcin.vercel.app');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
