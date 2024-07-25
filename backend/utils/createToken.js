506fb7027e0f6cddc1d72d64b64ace591b248d39
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-07-25 23:25:36 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: true,
-    sameSite: 'None',
+    sameSite: 'Lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
     partitioned: true  // Add this line
   });
